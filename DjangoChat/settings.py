d5790fd1cffe9867bd4fc4e9bd4f15b0f1846acb
dhwpdnr <ink1237@gmail.com>
2023-01-26 17:07:21 +0900

    system setting, index page
    create core app, create base.html with tailwind, add ASGI_APPLICATION setting in settings.py

--- DjangoChat/settings.py
+++ DjangoChat/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -68,6 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'DjangoChat.wsgi.application'
+ASGI_APPLICATION = 'DjangoChat.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
