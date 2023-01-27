0dbf5864d62684bbb7dc9821418b54de5b284dd5
dhwpdnr <ink1237@gmail.com>
2023-01-27 15:40:55 +0900

    [feat] save message
    create message model, create logic to save message in databasse

--- room/views.py
+++ room/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from .models import Room
+from .models import Room, Message
 
 
 @login_required
@@ -13,5 +13,6 @@ def rooms(request):
 @login_required
 def room(request, slug):
     room = Room.objects.get(slug=slug)
+    messages = Message.objects.filter(room=room)[0:25]
 
-    return render(request, 'room/room.html', {'room': room})
+    return render(request, 'room/room.html', {'room': room, 'messages': messages})
